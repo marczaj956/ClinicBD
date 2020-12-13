b1828f7750efc8bf7cc81c684c114c238ccadec2
Dominika Wysocka <dwysocka13@gmail.com>
2020-12-13 12:18:55 +0100

    kryteria do wyszukiwan badań

---
+++ MedicalClinic/MedicalClinic/ExaminationsSearchCriteria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalClinic
+{
+    class ExaminationsSearchCriteria
+    {
+        private int appointmentId;
+        private int patientId;
+
+        public int getAppointmentId()
+        {
+            return appointmentId;
+        }
+
+        public void setAppointmentId(int appointmentId)
+        {
+            this.appointmentId = appointmentId;
+        }
+        public int getPatientId()
+        {
+            return patientId;
+        }
+
+        public void setPatientId(int patientId)
+        {
+            this.patientId = patientId;
+        }
+       
+
+
+    }
+}
